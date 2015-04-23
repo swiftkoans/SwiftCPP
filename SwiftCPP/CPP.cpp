56fadbdedec2cbf60b5bab61ae1a9d6ac4fa658c
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-23 16:52:19 +0800

    C++ with Swift-C++ bridging header.

---
+++ SwiftCPP/CPP.cpp
@@ -0,0 +1,14 @@
+//
+//  CPP.cpp
+//  SwiftCPP
+//
+//  Created by Calvin Cheng on 23/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+#include "CPP.h"
+
+double timesTwo(double number)
+{
+    return 2 * number;
+}
---
+++ SwiftCPP/CPP.h
@@ -0,0 +1,24 @@
+//
+//  CPP.h
+//  SwiftCPP
+//
+//  Created by Calvin Cheng on 23/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+#ifndef __SwiftCPP__CPP__
+#define __SwiftCPP__CPP__
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    double timesTwo(double number);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* defined(__SwiftCPP__CPP__) */
